5e799c2e19cf83424ac3c2558e40f6d9cc16d3fb
Mike Houser <tharkad@casdra.com>
2019-06-27 13:21:21 -0700

    Removed ErrorBoundary example

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,11 +2,6 @@ import React from 'react';
 import personStyles from './Person.module.css';
 
 const person = (props) => {
-    const rnd = Math.random();
-    if (rnd > 0.7) {
-        throw new Error('test');
-    }
-
     return (
         <div className={personStyles.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
