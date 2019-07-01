d6d6e84995ad33602cf22f587784527e7ad1b8d4
Mike Houser <tharkad@casdra.com>
2019-07-01 13:46:18 -0700

    PropTypes demo

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,19 +2,30 @@ import React, { Component } from 'react';
 import Auxiliary from '../../../hoc/Auxiliary';
 import WithClass from '../../../hoc/WithClass';
 import personStyles from './Person.module.css';
+import PropTypes from 'prop-types';
 
 class Person extends Component{
-
     render () {
         console.log('[Person.js] render');
         return (
             <Auxiliary>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+                <input 
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}
+                />
             </Auxiliary>
         )
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default WithClass(Person, personStyles.Person);
