c7651406fae71740b17e1b8c6aabb95d84a7b685
Mike Houser <tharkad@casdra.com>
2019-07-01 13:12:01 -0700

    Using WithClass hoc component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Aux from '../../../hoc/Auxiliary';
+import WithClass from '../../../hoc/WithClass';
 import personStyles from './Person.module.css';
 
 class Person extends Component{
@@ -7,11 +7,11 @@ class Person extends Component{
     render () {
         console.log('[Person.js] render');
         return (
-            <Aux>
+            <WithClass classes = {personStyles.Person}>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </Aux>
+            </WithClass>
         )
     }
 }
