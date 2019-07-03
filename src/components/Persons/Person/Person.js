0f2147aea54805276b8b3ad6a70bd336a23701ab
Mike Houser <tharkad@casdra.com>
2019-07-03 10:58:08 -0700

    Refs in class based components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,6 +5,16 @@ import personStyles from './Person.module.css';
 import PropTypes from 'prop-types';
 
 class Person extends Component{
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+    
+    componentDidMount() {
+        //this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+    
     render () {
         console.log('[Person.js] render');
         return (
@@ -13,6 +23,8 @@ class Person extends Component{
                 <p>{this.props.children}</p>
                 <input 
                     type="text" 
+                    //ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
                     onChange={this.props.changed} 
                     value={this.props.name}
                 />
